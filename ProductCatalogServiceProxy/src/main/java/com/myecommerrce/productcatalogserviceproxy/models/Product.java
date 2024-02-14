a0626ed23bfd69492f30df068ed0afc203856a5a
sabyasachi ghosh <sabyasachi9733@gmail.com>
2024-02-13 21:07:41 -0800

    Models Created Successfully

--- ProductCatalogServiceProxy/src/main/java/com/myecommerrce/productcatalogserviceproxy/models/Product.java
+++ ProductCatalogServiceProxy/src/main/java/com/myecommerrce/productcatalogserviceproxy/models/Product.java
@@ -1,4 +1,14 @@
 package com.myecommerrce.productcatalogserviceproxy.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Product {
+    private String title;
+    private String description;
+    private String imageUrl;
+    private Double price;
+    private Category category;
 }
