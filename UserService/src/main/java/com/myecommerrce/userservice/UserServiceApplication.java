d697cf61b2ffd3355bf9e9659c9b62a875e558b8
sabyasachi ghosh <sabyasachi9733@gmail.com>
2024-03-08 00:27:32 +0530

    User Micro Service Added

---
+++ UserService/src/main/java/com/myecommerrce/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myecommerrce.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
